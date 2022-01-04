08c2c4ccee749a99528290c1d7f2878d66309bb7
alexandre-nunes <alexandre.santosnunes@gmail.com>
2022-01-04 07:07:59 -0300

    ajustes

--- cypress/support/pages/login-sistema/elements.js
+++ cypress/support/pages/login-sistema/elements.js
@@ -1,7 +1,7 @@
 export const ELEMENTS = {
     InputUsuarioSistema: ':nth-child(1) > .form-element > :nth-child(1) > .input-wrapper > .input-wrapper__text-input',
     InputSenhaSistema: ':nth-child(2) > .form-element > :nth-child(1) > .input-wrapper > .input-wrapper__text-input',
-    ButtonLoginSistema: '.c-internal-login__login-button',
+    ButtonLoginSistema: '.c-button',
     ValidaLogin: '.c-navbar__item > p',
     FecharBanner: '#onetrust-accept-btn-handler'
 }
